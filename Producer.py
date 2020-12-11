ab25c20a953fc83ec54ed74b1250b4bf3cf1e8f8
Phiphiste <phiram2306@gmail.com>
2020-12-11 23:26:05 +0100

    Update Producer.py and Consumer.py

--- Producer.py
+++ Producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-with open('/example/data/inputfile.txt') as f:
-     lines = f.readlines()
-
-for line in lines:
-   producer.send('test', line)
+#with open('inputext.txt') as f:
+ #    lines = f.readlines()
+print(producer.send('stopcovid',b'message').get(timeout=30))
+#for line in lines:
+#   producer.send('test', line)
